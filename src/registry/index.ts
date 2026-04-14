e702f6e5bc63743851ae7fa138e78ae01e5f63f9
teteudeveloper <teteudeveloper@gmail.com>
2026-04-14 18:11:19 -0300

    feat: add base64 encoder to tool registry

--- src/registry/index.ts
+++ src/registry/index.ts
@@ -1,8 +1,10 @@
 import type { Tool } from "../types/tools"
 import JsonFormatter from "../tools/json-formatter"
+import Base64Encoder from "../tools/base64-encoder"
 
 const tools: Tool[] = [
   JsonFormatter,
+  Base64Encoder,
 ]
 
 export function getTools(): Tool[] {
