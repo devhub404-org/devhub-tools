f2f59e21a672136e6e3c3afb903568826f694fa0
teteudeveloper <teteudeveloper@gmail.com>
2026-04-15 21:28:09 -0300

    feat: add password generator to tool registry

--- src/registry/index.ts
+++ src/registry/index.ts
@@ -1,12 +1,14 @@
 import type { Tool } from "../types/tools"
-import JsonFormatter from "../tools/json-formatter"
-import Base64Encoder from "../tools/base64-encoder"
-import UUIDGenerator from "../tools/uuid-generator"
+import JsonFormatter      from "../tools/json-formatter"
+import Base64Encoder      from "../tools/base64-encoder"
+import UUIDGenerator      from "../tools/uuid-generator"
+import PasswordGenerator  from "../tools/password-generator"
 
 const tools: Tool[] = [
   JsonFormatter,
   Base64Encoder,
   UUIDGenerator,
+  PasswordGenerator,
 ]
 
 export function getTools(): Tool[] {
